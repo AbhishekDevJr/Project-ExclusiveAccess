fe5ce1a7f4607357e604026f8aba72c9cf4e503d
Abhishek Choudhari <abhishek007coc@gmail.com>
2024-02-28 00:37:51 +0530

    CORS Origin url updated to Client Side URL

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://exclusiveaccess.netlify.app',
 }));
 app.use('/', indexRouter);
 app.use('/users', userRouter);
