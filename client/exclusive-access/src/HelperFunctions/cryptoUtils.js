d0138fd331dfeb25b35d084010c11be8bc159c60
abhishek <abhishek@finestardiamonds.com>
2024-03-06 18:07:57 +0530

    Edit/Delete Features in progress

--- client/exclusive-access/src/HelperFunctions/cryptoUtils.js
+++ client/exclusive-access/src/HelperFunctions/cryptoUtils.js
@@ -4,14 +4,14 @@ import { isObject } from 'lodash';
 const secretKey = 'mySecretKey';
 
 export const encryptData = (data) => {
-    if (data !== '') {
+    if (data && data !== '') {
         const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(data), secretKey).toString();
         return encryptedData;
     }
 };
 
 export const decryptData = (encryptedData) => {
-    if (encryptedData !== '') {
+    if (encryptedData && encryptedData !== '') {
         const decryptedBytes = CryptoJS.AES.decrypt(encryptedData, secretKey);
         const decryptedData = JSON.parse(decryptedBytes.toString(CryptoJS.enc.Utf8));
         return decryptedData;
